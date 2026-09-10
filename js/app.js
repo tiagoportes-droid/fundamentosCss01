219fb45ad15390edf2cfc11971d112a9b32613ae
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-09-10 12:49:45 -0300

    feat: add app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log(`Olá, Mundo.`);
+const apresentar = () => {
+  const resposta = document.getElementById("resp");
+  resposta.innerText = `Olá mundo.`;
+};
+
+const botao = document.getElementById("btnCompra");
+
+if (botao) {
+  botao.addEventListener("click", apresentar);
+}
